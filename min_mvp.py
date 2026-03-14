50e3561591c3665704459c10b73fbb0149c7d55e
Benedict von Schubert <benedictvonschubert@gmail.com>
2026-03-14 14:39:05 -0500

    removed old file

--- min_mvp.py
+++
@@ -1,9 +0,0 @@
-import ee
-ee.Authenticate()
-ee.Initialize(project='openearth-488015')
-
-no2 = (ee.ImageCollection("COPERNICUS/S5P/OFFL/L3_NO2")
-         .select('tropospheric_NO2_column_number_density')
-         .filterDate('2026-02-01', '2026-02-10'))
-
-print(no2)
